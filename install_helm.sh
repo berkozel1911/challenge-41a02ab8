a1f1e9253b6876948bb9975599d31839feed553b
Berk Özel <berkozel@protonmail.com>
2026-01-12 22:34:05 +0300

    Add better comments & installation warning messages.

--- install_helm.sh
+++ install_helm.sh
@@ -8,9 +8,17 @@ Starting Helm installer...
 
 EOF
 
-# Ensure prerequisite packages
+# Ensure prerequisite packages has been installed
 apt install -y curl gpg apt-transport-https
+
+echo "Installing Helm GPG keys..."
 curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | tee /usr/share/keyrings/helm.gpg > /dev/null
+
+echo "Adding Helm repository to /etc/apt/sources.list"
 echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt update
-sudo apt install helm
+
+echo "Running apt update"
+apt update
+
+echo "Installing Helm"
+apt -y install helm
