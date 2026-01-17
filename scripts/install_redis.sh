7b2d76fd3e2e952f6234132bef871e94c868773a
Berk Özel <berkozel@protonmail.com>
2026-01-17 19:44:54 +0300

    Take namespace name passed as variable.

--- scripts/install_redis.sh
+++ scripts/install_redis.sh
@@ -3,5 +3,5 @@
 set -e
 
 echo -e "\n[info] Installing Redis with Helm\n"
-helm install redis bitnami/redis -n ns0 -f $1 # Takes the first argument as Helm variable file.
+helm install redis bitnami/redis -n $NAMESPACE_ID -f $1 # Takes the first argument as Helm variable file.
 
