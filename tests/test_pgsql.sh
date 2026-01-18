c556d76ae2d71923aa21296360516761a0e4bf3c
Berk Özel <berkozel@protonmail.com>
2026-01-18 12:56:39 +0300

    Minor changes.

--- tests/test_pgsql.sh
+++ tests/test_pgsql.sh
@@ -4,12 +4,14 @@ set -e
 
 echo -e "\n[info] Testing PostgreSQL connection...\n"
 
-# TODO take port number from ymls.
-
+# Since this is a connection test from outside of the k3s cluster, 
+# -h argument takes the IP address of the running machine from the network interface.
+# e.g 192.168.1.10 instead of 127.0.0.1
 if pg_isready -U postgres -h $(hostname -I | awk '{ print $1 }') -p 30432; then
 	echo -e "\n[info] PostgreSQL is active and listening connections from outside of the k3s cluster."
 	exit 0;
 else
-	echo -e "\n[ERR] PostgreSQL connection test is failed."
+	echo -e "\n[ERROR] PostgreSQL connection test is failed."
+	exit 3
 fi
 
--- tests/test_redis.sh
+++ tests/test_redis.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-# TODO take port number from ymls
-# TODO take password from ymls
-# TODO add passed arguments number check
-
 echo -e "\n[info] Testing Redis connection...\n"
 
-if redis-cli -h $(hostname -I | awk '{ print $1 }') -p 30379 -a Password1. ping 2>/dev/null; then
+# Takes first argument ($1) as Redis root password.
+# Since this is a connection test from outside of the k3s cluster, 
+# -h argument takes the IP address of the running machine from the network interface.
+# e.g 192.168.1.10 instead of 127.0.0.1
+if redis-cli -h $(hostname -I | awk '{ print $1 }') -p 30379 -a $1 ping 2>/dev/null; then
 	echo -e "\n[info] Redis is active and listening connections from outside of the k3s cluster."
 else
-	echo -e "\n[ERR] Redis connection test is failed."
+	echo -e "\n[ERROR] Redis connection test is failed."
 fi
