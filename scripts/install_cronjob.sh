b13ea24951c5261f9523cc1e0ace58a2098a4dce
Berk Özel <berkozel@protonmail.com>
2026-01-18 21:11:17 +0300

    Update cronjob

--- scripts/install_cronjob.sh
+++ scripts/install_cronjob.sh
@@ -2,10 +2,9 @@
 
 set -e
 
-mkdir -p $PGSQL_BACKUP_DIR
-
-echo "kubectl exec -it postgres-postgresql-0 -n $NAMESPACE_ID -- pg_dump -U postgres -d $PGSQL_DB_NAME --file=/var/tmp/pgsql_backup.sql" > /usr/local/bin/backup_pgsql.sh
-echo "kubectl cp $NAMESPACE_ID/postgres-postgresql-0:/var/tmp/pgdump.sql PGSQL_BACKUP_DIR/pgsql_backup-$(date +%d_%m_%Y-%H_%M).sql" >> /usr/local/bin/backup_pgsql.sh
+echo "mkdir -p $PGSQL_BACKUP_DIR" > /usr/local/bin/backup_pgsql.sh
+echo "kubectl exec -it postgres-postgresql-0 -n $NAMESPACE_ID -- pg_dump postgresql://postgres:$PGSQL_ROOT_PW@127.0.0.1/$PGSQL_DB_NAME --file=/tmp/pgsql_dump.sql" >> /usr/local/bin/backup_pgsql.sh
+echo "kubectl cp $NAMESPACE_ID/postgres-postgresql-0:/tmp/pgsql_dump.sql $PGSQL_BACKUP_DIR/pgsql_backup-\$(date +%d_%m_%Y-%H_%M).sql" >> /usr/local/bin/backup_pgsql.sh
 
 chown -v root:root /usr/local/bin/backup_pgsql.sh 
 chmod 400 /usr/local/bin/backup_pgsql.sh
